5fc75171558b49cff049555f5c51b65191525ca8
pnhung177 <pnhung177@acegik.net>
2019-07-14 14:56:37 +0700

    Use the ServiceSelector to get the dynamic service methods (#4)

--- src/services/handler.js
+++ src/services/handler.js
@@ -42,8 +42,42 @@ function Handler(params = {}) {
   })
 
   const serviceResolver = config.serviceResolver || 'app-restfetch/resolver';
-  const serviceSelector = new ServiceSelector({ serviceResolver, sandboxRegistry });
-  // const serviceSelector = chores.newServiceSelector({ serviceResolver, sandboxRegistry });
+  let serviceSelector;
+  if (lodash.isFunction(chores.newServiceSelector)) {
+    serviceSelector = chores.newServiceSelector({ serviceResolver, sandboxRegistry });
+  } else {
+    const ServiceSelector = function (kwargs = {}) {
+      const { serviceResolver, sandboxRegistry } = kwargs;
+      assert.ok(this.constructor === ServiceSelector);
+      assert.ok(serviceResolver && lodash.isString(serviceResolver));
+      assert.ok(sandboxRegistry && lodash.isObject(sandboxRegistry));
+      let serviceResolverAvailable = true;
+      this.lookupMethod = function (serviceName, methodName) {
+        let ref = {};
+        if (serviceResolverAvailable) {
+          let resolver = sandboxRegistry.lookupService(serviceResolver);
+          if (resolver) {
+            ref.proxied = true;
+            ref.service = resolver.lookupService(serviceName);
+            if (ref.service) {
+              ref.method = ref.service[methodName];
+            }
+          } else {
+            serviceResolverAvailable = false;
+          }
+        }
+        if (!ref.method) {
+          ref.proxied = false;
+          ref.service = sandboxRegistry.lookupService(serviceName);
+          if (ref.service) {
+            ref.method = ref.service[methodName];
+          }
+        }
+        return ref;
+      }
+    }
+    serviceSelector = new ServiceSelector({ serviceResolver, sandboxRegistry });
+  }
 
   const ctx = { L, T, packageName, config, schemaManager, serviceSelector, oauthApi };
 
@@ -441,38 +475,4 @@ function parsePhoneNumber(phoneString, defaultCountryCode) {
 
 function isValidPhoneNumber(phoneString, defaultCountryCode) {
   return phoneUtil.isValidNumber(phoneUtil.parse(phoneString, defaultCountryCode));
-}
-
-function ServiceSelector(kwargs = {}) {
-  const { serviceResolver, sandboxRegistry } = kwargs;
-
-  assert.ok(this.constructor === ServiceSelector);
-  assert.ok(serviceResolver && lodash.isString(serviceResolver));
-  assert.ok(sandboxRegistry && lodash.isObject(sandboxRegistry));
-
-  let serviceResolverAvailable = true;
-
-  this.lookupMethod = function (serviceName, methodName) {
-    let ref = {};
-    if (serviceResolverAvailable) {
-      let resolver = sandboxRegistry.lookupService(serviceResolver);
-      if (resolver) {
-        ref.proxied = true;
-        ref.service = resolver.lookupService(serviceName);
-        if (ref.service) {
-          ref.method = ref.service[methodName];
-        }
-      } else {
-        serviceResolverAvailable = false;
-      }
-    }
-    if (!ref.method) {
-      ref.proxied = false;
-      ref.service = sandboxRegistry.lookupService(serviceName);
-      if (ref.service) {
-        ref.method = ref.service[methodName];
-      }
-    }
-    return ref;
-  }
 }
