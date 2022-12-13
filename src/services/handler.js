44f212992b60c573def9e007b19b079f702b78d8
devman <devman@code.com>
2022-12-13 10:32:38 +0700

    [sendOTP] Update appType, appPlatformType

--- src/services/handler.js
+++ src/services/handler.js
@@ -524,7 +524,7 @@ function generateOTP(packet = {}) {
 
 function sendOTP(packet = {}) {
   const { T, L } = packet;
-  const { packageName, config, serviceSelector, verification, skipped, options } = packet;
+  const { packageName, config, serviceSelector, verification, skipped, options, appType, appPlatformType } = packet;
   if (skipped === true) {
     return Promise.resolve(packet);
   }
@@ -537,6 +537,8 @@ function sendOTP(packet = {}) {
       tmpl: "An OTP has been created for phone: ${phoneNumber}"
     }));
     const msgInfo = {
+      appType,
+      appPlatformType,
       phoneNumber: verification.phoneNumber,
       otp: verification.otp,
       text: format(config.smsTemplate, { otp: verification.otp }),
